4003dbe821eb063dd12727007751021835325c66
Yassine Bekkali <yassine-bekkali@um5r.ac.ma>
2026-05-21 02:27:35 +0100

    Add files via upload

--- api/services.js
+++ api/services.js
@@ -2,9 +2,11 @@ import mysql from 'mysql2/promise';
 
 const db = () => mysql.createConnection({
   host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false },
 });
 
 export default async function handler(req, res) {
